aee8b96c29f1b44677b5d39b762bbd04ddf4fa5c
heewonkim-dev <heewon_kim@outlook.com>
2021-12-08 09:28:30 +0900

    FEAT: map 반응형 수정

--- client/src/routes/Map/index.js
+++ client/src/routes/Map/index.js
@@ -1,9 +1,19 @@
-import React, { useRef, useEffect } from 'react';
-import styled from 'styled-components/macro';
+import React, { useRef, useEffect, useState } from 'react';
 import MapHeader from 'components/Header/Homeheader';
 import { useDispatch } from 'react-redux';
 import { addMap } from 'redux/Slices/Map';
-import Example from './Example';
+import {
+  Btn,
+  MapMain,
+  MapContainer,
+  UserInfoContainer,
+  UserCard,
+  UserInfo,
+  Reply,
+  DogCard,
+} from './MapStyle';
+import CustomOverlayMap from 'components/CustomOverlay/CustomOverlayMap';
+import Walk from 'components/Overlay/Walk';
 
 function Index() {
   const mapRef = useRef(null);
@@ -26,12 +36,23 @@ function Index() {
     return () => {};
   }, []);
 
+  const [isWalkOpen, setIsWalkOpen] = useState(false);
+  const openWalkHandler = () => {
+    setIsWalkOpen(!isWalkOpen);
+    console.log('isWalkOpen', isWalkOpen);
+  };
+
   return (
     <>
       <MapHeader className="mapHeader" />
       <MapMain>
-        <MapContainer ref={mapRef} className="MapContainer"></MapContainer>
-
+        <MapContainer ref={mapRef} className="MapContainer">
+          {/* <CustomOverlayMap></CustomOverlayMap> */}
+          <Btn onClick={openWalkHandler} isWalkOpen={isWalkOpen}>
+            산책등록
+          </Btn>
+          {isWalkOpen === true ? <Walk></Walk> : null}
+        </MapContainer>
         <UserInfoContainer className="UserInfoContainer">
           <UserCard className="UserCard">
             <UserInfo className="flex-center-R UserInfo">hi</UserInfo>
@@ -42,77 +63,5 @@ function Index() {
     </>
   );
 }
-
-const MapMain = styled.main`
-  display: flex;
-  height: calc(100vh - 7rem);
-
-  & .MapContainer {
-    flex: 0.65;
-  }
-
-  & .UserInfoContainer {
-    flex: 0.35;
-  }
-
-  @media screen and (max-width: 1000px) {
-    flex-direction: column;
-    height: 102rem;
-
-    & .MapContainer {
-      min-height: 20rem;
-      max-height: 35rem;
-    }
-
-    & .UserInfoContainer {
-      flex: 1;
-    }
-  }
-
-  @media screen and (min-width: 1400px) {
-    & .MapContainer {
-      flex: 0.75;
-    }
-
-    & .UserInfoContainer {
-      flex: 0.25;
-    }
-  }
-`;
-
-const MapContainer = styled.div`
-  min-height: 50rem;
-`;
-
-const UserInfoContainer = styled.div`
-  background-color: ${({ theme }) => theme.colors.secondColor};
-`;
-
-//# When pin clicked
-const UserCard = styled.section`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  min-height: 50rem;
-  padding: 1.3rem;
-  background-color: white;
-
-  & .UserInfo {
-    background-color: white;
-    flex: 0.3;
-  }
-
-  & .Reply {
-    background-color: red;
-    flex: 0.7;
-  }
-`;
-
-const UserInfo = styled.div``;
-
-const Reply = styled.div``;
-
-//# Before pin clicked
-const DogCard = styled.section``;
 
 export default Index;
